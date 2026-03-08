def0baec9628e6ff6aaaa429c65fdc44e8d10819
Ayden Greenan <79679437+DrakoHyena@users.noreply.github.com>
2026-03-07 20:52:07 -0500

    Add blink plugin configuration in blink.lua

---
+++ lua/drako/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+    "saghen/blink.cmp",
+    build = "cargo build --release",
+    dependencies = "rafamadriz/friendly-snippets",
+    opts = {
+        -- 'default' for mappings similar to 'supertab'
+        keymap = { preset = "super-tab" },
+        appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = "mono",
+        },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+    },
+}
