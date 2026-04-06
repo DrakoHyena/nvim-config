a615e5c1624a05888c0d3f26339bbd395fdf582d
Drako Hyena <79679437+DrakoHyena@users.noreply.github.com>
2026-04-05 22:15:26 -0400

    Fixed blink.cmp

--- lua/drako/plugins/lazy-lisp.lua
+++ lua/drako/plugins/lazy-lisp.lua
@@ -1,10 +1,12 @@
 return {
-  "dundalek/lazy-lsp.nvim",
-  dependencies = { "neovim/nvim-lspconfig" },
-  config = function()
-    require("lazy-lsp").setup({
-	use_vim_lsp_config = true
-    }) 
-  end,
+    "dundalek/lazy-lsp.nvim",
+    dependencies = {
+        "neovim/nvim-lspconfig",
+        "saghen/blink.cmp",
+    },
+    config = function()
+        require("lazy-lsp").setup({
+            use_vim_lsp_config = true
+        })
+    end,
 }
-
