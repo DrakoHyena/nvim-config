17b16bcb026b0bc34f2d1ab591cbe15a156795f1
Drako Hyena <79679437+DrakoHyena@users.noreply.github.com>
2026-04-06 20:03:56 -0400

    netrw improve, new theme, sidebar

--- lua/drako/init.lua
+++ lua/drako/init.lua
@@ -2,19 +2,26 @@ require("drako.remap")
 require("drako.lazy_init")
 
 -- telescope config
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>saf', builtin.find_files, { desc = 'Telescope files search' })
-vim.keymap.set('n', '<leader>sgf', builtin.git_files, { desc = 'Telescope git files search' })
-vim.keymap.set('n', '<leader>slg', builtin.live_grep, { desc = 'Telescope grep search' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>saf", builtin.find_files, { desc = "Telescope files search" })
+vim.keymap.set("n", "<leader>sgf", builtin.git_files, { desc = "Telescope git files search" })
+vim.keymap.set("n", "<leader>slg", builtin.live_grep, { desc = "Telescope grep search" })
 
 -- Harpoon
 local harpoon = require("harpoon")
 harpoon:setup()
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon add file" })
-vim.keymap.set("n", "<leader>q", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-    { desc = "Harpoon toggle menu" })
-vim.keymap.set("n", "<leader>hn", function() harpoon:list():prev() end, { desc = "Harpoon navigate next entry" })
-vim.keymap.set("n", "<leader>hp", function() harpoon:list():next() end, { desc = "Harpoon navigate previous entry" })
+vim.keymap.set("n", "<leader>ha", function()
+	harpoon:list():add()
+end, { desc = "Harpoon add file" })
+vim.keymap.set("n", "<leader>q", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Harpoon toggle menu" })
+vim.keymap.set("n", "<leader>hn", function()
+	harpoon:list():prev()
+end, { desc = "Harpoon navigate next entry" })
+vim.keymap.set("n", "<leader>hp", function()
+	harpoon:list():next()
+end, { desc = "Harpoon navigate previous entry" })
 
 -- Treesitter
 -- See ./plugins/treesitter.lua for config
@@ -22,8 +29,21 @@ vim.keymap.set("n", "<leader>hp", function() harpoon:list():next() end, { desc =
 -- Fugitive
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
+-- netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 8
+vim.keymap.set("n", "<leader>sb", ":Lexplore<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>wd", "<C-w>>", { desc = "Make window bigger" })
+vim.keymap.set("n", "<leader>wa", "<C-w><", { desc = "Make window smaller" })
+vim.keymap.set("n", "<CR>", ":<up>", { silent = false, noremap = true })
+
 -- Theme/Colors
-vim.cmd('colorscheme industry')
+-- see ./plugins/vauge.nvim
 
 -- Misc Editor Settings
 vim.opt.guicursor = ""
