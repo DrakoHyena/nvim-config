4f5567773c3028f802abf5592a7eebdf9593ac12
Drako Hyena <79679437+DrakoHyena@users.noreply.github.com>
2026-05-18 14:25:03 -0400

    Changed theme2

--- lua/drako/plugins/theme.lua
+++ lua/drako/plugins/theme.lua
@@ -1,11 +1,6 @@
 return {
-	"sainnhe/gruvbox-material",
+	"craftzdog/solarized-osaka.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		-- Optionally configure and load the colorscheme
-		-- directly inside the plugin declaration.
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme("gruvbox-material")
-	end,
+	opts = {},
 }
